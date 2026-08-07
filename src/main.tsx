0edc4fdce3dd6c95cb2020dff7217fbd4c8bd6a5
Lunixizm <github@lunixizm.website>
2026-08-07 23:44:30 +0300

    feat: use youtube-nocookie embed and clean up console warnings

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { StyleSheetManager } from "styled-components";
 import App from "./App";
+
+const CUSTOM_PROPS = new Set([
+  "mobileExpanded",
+  "active",
+  "maximized",
+  "isTransforming",
+  "zIndex",
+  "variant",
+  "pos",
+  "delay",
+  "renk",
+  "ust",
+  "bos",
+  "calisma",
+  "tatil",
+  "bugun",
+  "secili",
+  "dolu",
+  "marginY",
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <StyleSheetManager shouldForwardProp={(prop) => !CUSTOM_PROPS.has(prop)}>
+      <App />
+    </StyleSheetManager>
   </React.StrictMode>
 );
